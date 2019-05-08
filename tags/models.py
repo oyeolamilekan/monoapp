5b0210fef2a7401cdd3006c0b87630d0bc30e251
oyejohnson <johnsonoye34@gmail.com>
2019-05-09 00:25:26 +0100

    added the parent tag

--- tags/models.py
+++ tags/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
-
+class TagsField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def get_prep_value(self, value):
+        return str(value).lower()
+    
 class Catergories(models.Model):
     '''
         Parent Catergories for shop owners
@@ -10,7 +16,7 @@ class Catergories(models.Model):
 
     title = models.CharField(max_length=200)
     objects = models.Manager()
-    tags = ArrayField(models.CharField(max_length=200))
+    tags = ArrayField(TagsField(max_length=200))
 
     def __str__(self):
         return '{title} - {tags}'.format(title=self.title, tags=self.tags)
