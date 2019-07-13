ed236103074ceeca5625d188461ba40212f32b48
oyejohnson <johnsonoye34@gmail.com>
2019-07-13 18:07:34 +0100

    serverl

--- website/settings.py
+++ website/settings.py
@@ -113,7 +113,7 @@ CLOUDINARY_STORAGE = {
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 if platform.system() == 'Linux':
-    DEBUG = False
+    DEBUG = True
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
