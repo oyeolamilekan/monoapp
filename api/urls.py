1d991fd9dd2bdc7b8b3ab2e82fd11496dea53a87
oyejohnson <johnsonoye34@gmail.com>
2019-07-13 18:56:42 +0100

    hope it worksl

--- api/urls.py
+++ api/urls.py
@@ -2,55 +2,22 @@
     Handles the url path for the entire app.
 '''
 
-from .urls_route.auth import AUTH_URL
-from .urls_route.commerce import COMMERCE_URL
-from .urls_route.store import STORE_URL
-from .urls_route.feedback import CREATE_FEEDBACK
-from .urls_route.lesson import LESSON_URL
-from .urls_route.comments import COMMENT_URL
-from .urls_route.analytics import ANALYTICS
-from .views import analytics
-from django.urls import path
-
+from api.urls_route.analytics import ANALYTICS_URL
+from api.urls_route.auth import AUTH_URL
+from api.urls_route.comments import COMMENT_URL
+from api.urls_route.commerce import COMMERCE_URL
+from api.urls_route.feedback import CREATE_FEEDBACK
+from api.urls_route.lesson import LESSON_URL
+from api.urls_route.store import STORE_URL
 
 app_name = 'api'
 
 urlpatterns = [
-    path(
-        "create_analytics_product/<slug:pk>/",
-        analytics.create_product_analytics,
-        name="create_product_analytics",
-    ),
-    path(
-        "create_shop_analytics/<slug:pk>/",
-        analytics.create_shop_analytics,
-        name="create_shop_analytics",
-    ),
-    path(
-        "create_tags_analytics/",
-        analytics.create_tags_analytics,
-        name='create_tags_analytics'
-    ),
-    path(
-        "get_data/<slug:pk>/",
-        analytics.get_product_clicks, 
-        name="get_data"
-    ),
-    path(
-        "get_shop_view/<slug:pk>/", 
-        analytics.get_shop_views, 
-        name="get_shop_view"
-    ),
-    path(
-        "get_products_clicked/",
-        analytics.get_product_clicked,
-        name='get_products'
-    ),
     *AUTH_URL,
+    *ANALYTICS_URL,
     *COMMERCE_URL,
     *CREATE_FEEDBACK,
     *COMMENT_URL,
-    *ANALYTICS,
     *LESSON_URL,
     *STORE_URL,
 ]
