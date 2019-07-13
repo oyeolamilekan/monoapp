d60007f37e8c5fe2a26e857fb8f496659ec52c0a
oyejohnson <johnsonoye34@gmail.com>
2019-07-13 20:24:56 +0100

    ffl

--- api/urls.py
+++ api/urls.py
@@ -9,22 +9,15 @@ from api.urls_route.commerce import COMMERCE_URL
 from api.urls_route.feedback import CREATE_FEEDBACK
 from api.urls_route.lesson import LESSON_URL
 from api.urls_route.store import STORE_URL
-from .views import analytics
-from django.urls import path
 
 app_name = 'api'
 
 urlpatterns = [
-    path(
-        "mook/",
-        analytics.create_tags_analytics,
-        name='create_tags_analytics'
-    ),
-    *AUTH_URL,
-    *ANALYTICS_URL,
-    *COMMERCE_URL,
-    *CREATE_FEEDBACK,
-    *COMMENT_URL,
-    *LESSON_URL,
-    *STORE_URL,
+    # *AUTH_URL,
+    # *ANALYTICS_URL,
+    # *COMMERCE_URL,
+    # *CREATE_FEEDBACK,
+    # *COMMENT_URL,
+    # *LESSON_URL,
+    # *STORE_URL,
 ]
