885386196c1f9a507012256e1f6e7e9ab31978c2
oyejohnson <johnsonoye34@gmail.com>
2019-07-13 18:46:43 +0100

    hope it works

--- api/urls_route/analytics.py
+++ api/urls_route/analytics.py
@@ -3,34 +3,5 @@ from django.urls import path
 from ..views import analytics
 
 ANALYTICS = [
-    path(
-        "create_analytics_product/<slug:pk>/",
-        analytics.create_product_analytics,
-        name="create_product_analytics",
-    ),
-    path(
-        "create_shop_analytics/<slug:pk>/",
-        analytics.create_shop_analytics,
-        name="create_shop_analytics",
-    ),
-    path(
-        "create_tags_analytics/",
-        analytics.create_tags_analytics,
-        name='create_tags_analytics'
-    ),
-    path(
-        "get_data/<slug:pk>/",
-        analytics.get_product_clicks, 
-        name="get_data"
-    ),
-    path(
-        "get_shop_view/<slug:pk>/", 
-        analytics.get_shop_views, 
-        name="get_shop_view"
-    ),
-    path(
-        "get_products_clicked/",
-        analytics.get_product_clicked,
-        name='get_products'
-    )
+    
 ]
